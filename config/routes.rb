aaf015a06a244aef9270e0c1f8730fbcba0b2500
Tiara Italyana Putri Pribadi <tiaraitalyana@gmail.com>
2019-11-27 20:20:27 +0700

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  if Rails.env.development?
-    # mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-    mount GraphqlPlayground::Rails::Engine, at: "/graphql_playground", graphql_path: "/graphql"
-  end
+  # mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  mount GraphqlPlayground::Rails::Engine, at: "/graphql_playground", graphql_path: "/graphql"
   post "/graphql", to: "graphql#execute"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
