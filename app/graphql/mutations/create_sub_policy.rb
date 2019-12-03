09136d56e67e9af27db845644c4c49b977f525cf
djarkobobanov <djarkobobanov@gmail.com>
2019-12-03 18:22:15 +0700

    Melengkapi kekurangan yang sebelumnya

--- app/graphql/mutations/create_sub_policy.rb
+++ app/graphql/mutations/create_sub_policy.rb
@@ -4,17 +4,19 @@ module Mutations
     argument :title, String, required: true
     argument :description, String, required: true
     argument :parent_id, ID, required: true
+    argument :reference_ids, [ID], required: false
 
 
     # return type from the mutation
     field :policy, Types::PolicyType, null: true
 
-    def resolve(parent_id:, title: nil, description: nil)
+    def resolve(parent_id:, title: nil, description: nil, reference_ids: nil)
       policy = Policy.create!(
         title: title,
         description: description,
         parent: Policy.find(parent_id),
-        user_id: context[:current_user].id
+        user_id: context[:current_user].id,
+        reference_ids: reference_ids
       )
       {policy: policy}
     end
