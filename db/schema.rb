20f7f76a0192ea2fce015ee8fa8bf2648d857003
djarkobobanov <djarkobobanov@gmail.com>
2019-12-03 18:35:59 +0700

    Remove fte_estimate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_064732) do
+ActiveRecord::Schema.define(version: 2019_12_03_113347) do
 
   create_table "business_processes", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.text "name"
@@ -54,7 +54,6 @@ ActiveRecord::Schema.define(version: 2019_12_03_064732) do
     t.text "assertion"
     t.text "ipo"
     t.text "control_owner"
-    t.integer "fte_estimate"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
