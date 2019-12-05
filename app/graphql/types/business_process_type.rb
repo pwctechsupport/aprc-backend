4e9079b9dda36c55abd39c604f9b6f51b13bd8cb
Tiara Italyana Putri Pribadi <tiaraitalyana@gmail.com>
2019-12-05 16:52:24 +0700

    update business process type

--- app/graphql/types/business_process_type.rb
+++ app/graphql/types/business_process_type.rb
@@ -4,5 +4,8 @@ module Types
     field :name, String, null: false
     field :ancestry, ID, null: true
     field :parent_id, ID, null: true
+    field :parent, Types::BusinessProcessType, null: true
+    field :children, [Types::BusinessProcessType], null: true
+    field :ancestors, [Types::BusinessProcessType], null: true
   end
 end
