cec793b84a8f87ebfde149f13616d9f9ca0df93c
Tiara Italyana Putri Pribadi <tiaraitalyana@gmail.com>
2019-12-05 19:16:28 +0700

    update policies

--- app/graphql/types/resource_type.rb
+++ app/graphql/types/resource_type.rb
@@ -4,7 +4,7 @@ module Types
     field :name, String, null: false
     field :resuploadUrl, String, null: true
     field :category, String, null: true
-    field :policy_ids, [Types::PolicyType], null: true
+    field :policy_ids, [ID], null: true
     field :policies, [Types::PolicyType], null: true
     field :control_ids, [Types::ControlType], null: true
     field :controls, [Types::ControlType], null: true
@@ -12,11 +12,6 @@ module Types
     field :business_process, Types::BusinessProcessType, null: true
     def  resupload_url
       attachment = object.resupload.url
-    end
-
-    def policies
-      current_user = context[:current_user]
-      current_user.policies
     end
   end
 end
