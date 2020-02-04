f09c008d19fb349281e39d7160b813c7f357ad6c
Tiara Italyana Putri Pribadi <tiaraitalyana@gmail.com>
2020-02-04 14:50:07 +0700

    default url options

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => 'mandalorian.rubyh.co' }
+  config.action_mailer.default_url_options = { :host => 'skywalker.rubyh.co' }
   config.action_mailer.asset_host = 'https://mandalorian.rubyh.co'
 
   config.action_mailer.delivery_method = :smtp
