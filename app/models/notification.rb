1fdf2826d0b10a96ff808b26178028e613729f8e
fawwazfauzi <fawwaz@rubyh.co>
2020-03-09 13:34:07 +0700

    notif

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -25,7 +25,7 @@ class Notification < ApplicationRecord
 
   def self.send_notification_to_all(arr_of_user, title, body, originator,sender_user_id, data_type= nil)
     arr_of_user.each do |user|
-      Notification.create(user_id: user&.to_i, title: title, body: body , originator: originator, data: originator.to_j\son, sender_user_id: sender_user_id, data_type: data_type, is_general: true)
+      Notification.create(user_id: user&.to_i, title: title, body: body , originator: originator, data: originator.to_json, sender_user_id: sender_user_id, data_type: data_type, is_general: true)
     end
   end
 end
