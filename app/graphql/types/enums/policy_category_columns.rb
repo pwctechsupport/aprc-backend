0c2295122cb77af3304e3208767434c6e0560ed8
fawwazfauzi <fawwaz@rubyh.co>
2020-05-06 16:55:45 +0700

    Change enuum in resolvers fix

--- app/graphql/types/enums/policy_category_columns.rb
+++ app/graphql/types/enums/policy_category_columns.rb
@@ -1,4 +1,14 @@
 class Types::Enums::PolicyCategoryColumns < Types::BaseEnum
-  PolicyCategory.column_names.each {|x| value x, x}
+  value "name","name"
+  value "created_at","created_at"
+  value "updated_at","updated_at"
+  value "draft_id","draft_id"
+  value "published_at","published_at"
+  value "trashed_at","trashed_at"
+  value "user_reviewer_id","user_reviewer_id"
+  value "status","status"
+  value "created_by","created_by"
+  value "last_updated_by","last_updated_by"
+  value "policy","policy"
   
 end
