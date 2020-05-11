6b15cb2c23a2ff280ac17b52101af2ec44ad3d98
fawwazfauzi <fawwaz@rubyh.co>
2020-05-11 16:25:42 +0700

    Verify authenticity token

--- app/graphql/mutations/update_policy.rb
+++ app/graphql/mutations/update_policy.rb
@@ -38,7 +38,7 @@ module Mutations
           policy&.attributes = args
           policy&.save_draft
         end
-      elsif args[:resource_ids].present? && (args.length == 3)
+      elsif args[:resource_ids].present? && args.length == 2
         policy.update(resource_ids: args[:resource_ids])
       else  
         raise GraphQL::ExecutionError, "Request not granted. Please Check Your Request Status"
