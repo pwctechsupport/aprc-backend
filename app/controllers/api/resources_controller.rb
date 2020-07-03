8dc894cdb209a6f7d75391618dde7cf867652340
fawwazfauzi <fawwaz@rubyh.co>
2020-07-03 15:51:22 +0700

    import controller leftout

--- app/controllers/api/resources_controller.rb
+++ app/controllers/api/resources_controller.rb
@@ -1,8 +1,9 @@
 module Api
   class ResourcesController < ApiController
     def import
-      if Resource.import(params[:file])
-      	render json: { status: 200, message: "Succesfully import file business process"}
+      status, error_data = Resource.import(params[:file])
+      if status
+      	render json: { status: 200, message: "Succesfully import file business process", error_data: error_data.as_json}
       else
       	render json: { status: 422, message: "Failed import file business process"}
       end
