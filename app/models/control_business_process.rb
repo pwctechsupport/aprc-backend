f42ee4824790e33a4374765c81580d88efd8a8b3
fawwazfauzi <fawwaz@rubyh.co>
2020-07-17 15:17:06 +0700

    PWC version History fix

--- app/models/control_business_process.rb
+++ app/models/control_business_process.rb
@@ -1,11 +1,11 @@
 class ControlBusinessProcess < ApplicationRecord
-  has_paper_trail on: []
+  # has_paper_trail on: []
 
-  # Add callbacks in the order you need.
-  paper_trail.on_destroy    # add destroy callback
-  paper_trail.on_update     # etc.
-  paper_trail.on_create
-  paper_trail.on_touch
+  # # Add callbacks in the order you need.
+  # paper_trail.on_destroy    # add destroy callback
+  # paper_trail.on_update     # etc.
+  # paper_trail.on_create
+  # paper_trail.on_touch
 
   has_drafts
 
