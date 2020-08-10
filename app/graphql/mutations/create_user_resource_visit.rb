f58d70d42ab30f3f392c6ab2b084019cd99a1e02
fawwazfauzi <fawwaz@rubyh.co>
2020-08-10 21:48:53 +0700

    SQLi fix for visits

--- app/graphql/mutations/create_user_resource_visit.rb
+++ app/graphql/mutations/create_user_resource_visit.rb
@@ -13,9 +13,9 @@ module Mutations
       current_user = context[:current_user]
       args[:user_id] = current_user&.id
       args[:recent_visit] = Time.now
-      if UserResourceVisit.find_by(user_id: current_user&.id ,resource_id: args[:resource_id]).present?
-        user_resource_visit =  UserResourceVisit.find_by(user_id: current_user&.id ,resource_id: args[:resource_id])
-        user_resource_visit&.update(recent_visit: args[:recent_visit])
+      user_resource_visit = current_user&.resource_visits&.where(["resource_id = ?", "#{args[:resource_id]}"])
+      if user_resource_visit.present?
+        user_resource_visit&.first&.update!(recent_visit: args[:recent_visit])
       else
         user_resource_visit = UserResourceVisit.create!(args.to_h)
       end
