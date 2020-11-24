ddb3e2db884bb2bb1248dac0f89d5aeb0cfda2d9
Tiara Italyana Putri Pribadi <tiaraitalyana@gmail.com>
2020-11-24 13:34:39 +0700

    add scope policy released

--- app/graphql/resolvers/query_type/user_policies_resolver.rb
+++ app/graphql/resolvers/query_type/user_policies_resolver.rb
@@ -8,7 +8,11 @@ module Resolvers
 
       def resolve(filter:, page: nil,limit: nil)
         Policy.page(page).per(limit)
-        @q = Policy.ransack(filter.as_json)
+        if context[:current_user].has_role?(:user)
+          @q = Policy.released.ransack(filter.as_json)
+        else
+          @q = Policy.ransack(filter.as_json)
+        end
         @q.sorts = 'title asc' if @q.sorts.empty?
         @q.result(distinct: true).page(page).per(limit)
         # ::context[:current_user].page(page).per(limit)
