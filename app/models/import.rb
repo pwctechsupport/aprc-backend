751be49c97ac85fc844dc24e14496aef22dbe7a9
Tiara Italyana Putri Pribadi <tiaraitalyana@gmail.com>
2020-11-30 11:44:51 +0700

    add validates content type import model

--- app/models/import.rb
+++ app/models/import.rb
@@ -2,6 +2,7 @@ class Import < ApplicationRecord
   has_paper_trail
 
   has_attached_file :file
+  validates_attachment_content_type :file, content_type: { content_type: ["image/jpeg", "image/gif", "image/png", "application/pdf", "application/xlsx","application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "text/plain", "application/zip", "application/x-zip", "application/x-zip-compressed","application/octet-stream","application/vnd.ms-office","application/vnd.openxmlformats-officedocument.presentationml.presentation", "application/vnd.ms-powerpoint"] }
 
   def to_humanize
     "#{self.name} : #{self.file_file_name}"
