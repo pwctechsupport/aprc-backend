cca220898d6ea2deb47e51ed2329fc55c134fc9c
Tiara Italyana Putri Pribadi <tiaraitalyana@gmail.com>
2020-12-02 12:17:32 +0700

    trying import log

--- app/controllers/api/policy_categories_controller.rb
+++ app/controllers/api/policy_categories_controller.rb
@@ -2,11 +2,11 @@ module Api
   class PolicyCategoriesController < ApiController
     def import
       status, error_data = current_user.present? ? PolicyCategory.import(params[:file], current_user) : PolicyCategory.import(params[:file])
-      Import.create(name: "Policy Category", file: params[:file])
+      im = Import.create(name: "Policy Category", file: params[:file])
       if status
-      	render json: { status: 200, message: "Succesfully import file risk", error_data: error_data.as_json}
+      	render json: { status: 200, message: "Succesfully import file risk. #{im.errors.full_messages.full_messages.join(', ')}", error_data: error_data.as_json}
       else
-      	render json: { status: 422, message: "Failed import file risk"}
+      	render json: { status: 422, message: "Failed import file risk. #{im.errors.full_messages.full_messages.join(', ')}"}
       end
     end
   end
