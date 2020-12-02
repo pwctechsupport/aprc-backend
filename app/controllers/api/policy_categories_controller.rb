452e1c662bb20d68d367150f25bc5e1984370b6b
Tiara Italyana Putri Pribadi <tiaraitalyana@gmail.com>
2020-12-02 12:23:47 +0700

    typo fixing messages

--- app/controllers/api/policy_categories_controller.rb
+++ app/controllers/api/policy_categories_controller.rb
@@ -4,9 +4,9 @@ module Api
       status, error_data = current_user.present? ? PolicyCategory.import(params[:file], current_user) : PolicyCategory.import(params[:file])
       im = Import.create(name: "Policy Category", file: params[:file])
       if status
-      	render json: { status: 200, message: "Succesfully import file risk. #{im.errors.full_messages.full_messages.join(', ')}", error_data: error_data.as_json}
+      	render json: { status: 200, message: "Succesfully import file risk. #{im.errors.full_messages.join(', ')}", error_data: error_data.as_json}
       else
-      	render json: { status: 422, message: "Failed import file risk. #{im.errors.full_messages.full_messages.join(', ')}"}
+      	render json: { status: 422, message: "Failed import file risk. #{im.errors.full_messages.join(', ')}"}
       end
     end
   end
