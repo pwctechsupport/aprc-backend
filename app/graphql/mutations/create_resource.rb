910c6cfadd60bf4b756cf57cdde997623868599a
Tiara Italyana Putri Pribadi <tiaraitalyana@gmail.com>
2020-12-03 15:57:13 +0700

    remove skip callback

--- app/graphql/mutations/create_resource.rb
+++ app/graphql/mutations/create_resource.rb
@@ -90,9 +90,7 @@ module Mutations
       if args[:resupload].present?
         args.trust
         args[:resupload_file_name] = "#{ConvertName.raw(args[:name])}#{Resource.resource_file_type(resource).html_safe}"
-        Resource.skip_callbacks = true
-        resource.update_attributes(resupload: args[:resupload], resupload_file_name: args[:resupload_file_name], base_64_file: args[:resupload])
-        Resource.skip_callbacks = false
+        resource.update_column(resupload: args[:resupload], resupload_file_name: args[:resupload_file_name], base_64_file: args[:resupload])
       end
       admin = User.with_role(:admin_reviewer).pluck(:id)
       if resource.id.present?
