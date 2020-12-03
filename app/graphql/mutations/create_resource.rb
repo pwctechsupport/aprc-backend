95ff5d0814dc293d94102df3a2ac9e5bdb1f4941
Tiara Italyana Putri Pribadi <tiaraitalyana@gmail.com>
2020-12-03 16:02:29 +0700

    use validate false for update resource

--- app/graphql/mutations/create_resource.rb
+++ app/graphql/mutations/create_resource.rb
@@ -90,7 +90,10 @@ module Mutations
       if args[:resupload].present?
         args.trust
         args[:resupload_file_name] = "#{ConvertName.raw(args[:name])}#{Resource.resource_file_type(resource).html_safe}"
-        resource.update_column(resupload: args[:resupload], resupload_file_name: args[:resupload_file_name], base_64_file: args[:resupload])
+        resource.resupload = args[:resupload]
+        resource.resupload_file_name = args[:resupload_file_name]
+        resource.base_64_file = args[:resupload])
+        resource.save(:validate => false)
       end
       admin = User.with_role(:admin_reviewer).pluck(:id)
       if resource.id.present?
