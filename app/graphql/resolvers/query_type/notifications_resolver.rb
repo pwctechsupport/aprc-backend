250c88f97d841061d09e1e3cab2bf8a463972ffa
Tiara Italyana Putri Pribadi <tiaraitalyana@gmail.com>
2020-12-21 16:49:20 +0700

    fixing bug notification resolver

--- app/graphql/resolvers/query_type/notifications_resolver.rb
+++ app/graphql/resolvers/query_type/notifications_resolver.rb
@@ -8,10 +8,14 @@ module Resolvers
 
       def resolve(filter:, page: nil, limit: nil)
         current_user = context[:current_user]
-        if filter[:title_or_originator_type_or_sender_user_name_cont]&.downcase.include?("system")
-          @q = Notification.where(user_id: current_user&.id, is_general: true).ransack(filter.as_json)
+        if filter[:title_or_originator_type_or_sender_user_name_cont].present?
+          if filter[:title_or_originator_type_or_sender_user_name_cont]&.downcase.include?("system")
+            @q = Notification.where(user_id: current_user&.id, is_general: true).ransack(filter.as_json)
+          else
+            @q = Notification.where(user_id: current_user&.id, is_general: false).ransack(filter.as_json)
+          end
         else
-          @q = Notification.where(user_id: current_user&.id, is_general: false).ransack(filter.as_json)
+          @q = Notification.where(user_id: current_user&.id).ransack(filter.as_json)
         end
         @q.sorts = 'created_at desc' if @q.sorts.empty?
         @q.result(distinct: true).page(page).per(limit)
