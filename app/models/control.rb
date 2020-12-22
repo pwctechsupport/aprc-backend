6d6b46dc23c8ccbb7780773f2e9ea79204447826
Tiara Italyana Putri Pribadi <tiaraitalyana@gmail.com>
2020-12-22 12:17:34 +0700

    remove uniq

--- app/models/control.rb
+++ app/models/control.rb
@@ -13,7 +13,7 @@ class Control < ApplicationRecord
   has_many :descriptions, through: :control_descriptions
   has_many :control_risks, class_name: "ControlRisk", foreign_key: "control_id", dependent: :destroy
   
-  has_many :risks, through: :control_risks, :uniq => true
+  has_many :risks, through: :control_risks
   has_many :resource_controls, dependent: :destroy
   has_many :resources, through: :resource_controls
   has_many :policy_controls, dependent: :destroy
