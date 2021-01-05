5734bf7773a095895780f1675774393b58695243
Tiara Italyana Putri Pribadi <tiaraitalyana@gmail.com>
2021-01-05 11:46:43 +0700

    fixing bug #PWC-803

--- app/graphql/resolvers/query_type/reviewer_policies_status_resolver.rb
+++ app/graphql/resolvers/query_type/reviewer_policies_status_resolver.rb
@@ -10,30 +10,30 @@ module Resolvers
         Policy.page(page).per(limit)
         @q = Policy.ransack(filter.as_json)
         
-        @q1 = @q.result.where(status:"waiting_for_review")
-        @q1 = @q1.sort_by(&:updated_at).reverse
+        # @q1 = @q.result.where(status:"waiting_for_review")
+        # @q1 = @q1.sort_by(&:updated_at).reverse
 
-        @q2 = @q.result.where(status:"waiting_for_approval")
-        @q2 = @q2.sort_by(&:updated_at).reverse
+        # @q2 = @q.result.where(status:"waiting_for_approval")
+        # @q2 = @q2.sort_by(&:updated_at).reverse
         
-        @q3 = @q.result.where(status:"ready_for_edit")
-        @q3 = @q3.sort_by(&:updated_at).reverse
+        # @q3 = @q.result.where(status:"ready_for_edit")
+        # @q3 = @q3.sort_by(&:updated_at).reverse
 
-        @q4 = @q.result.where(status:"draft")
-        @q4 = @q4.sort_by(&:updated_at).reverse
+        # @q4 = @q.result.where(status:"draft")
+        # @q4 = @q4.sort_by(&:updated_at).reverse
 
-        @q5 = @q.result.where(status:"release")
-        @q5 = @q5.sort_by(&:updated_at).reverse
+        # @q5 = @q.result.where(status:"release")
+        # @q5 = @q5.sort_by(&:updated_at).reverse
 
-        @q6 = @q.result.where(status:nil)
-        @q6 = @q6.sort_by(&:updated_at).reverse
+        # @q6 = @q.result.where(status:nil)
+        # @q6 = @q6.sort_by(&:updated_at).reverse
 
-        @q_final = @q1.push(*@q2,*@q3,*@q4,*@q5, *@q6)
-        ids = @q_final.map(&:id)
-        if ids.count != 0
-          @q = Policy.where(id: ids).order("FIELD(id, #{ids.join(',')})").all.ransack
-        end
-        @q.result(distinct: true).page(page).per(limit)
+        # @q_final = @q1.push(*@q2,*@q3,*@q4,*@q5, *@q6)
+        # ids = @q_final.map(&:id)
+        # if ids.count != 0
+        #   @q = Policy.where(id: ids).order("FIELD(id, #{ids.join(',')})").all.ransack
+        # end
+        @q.result(distinct: true).order(status: :desc, updated_at: :desc).page(page).per(limit)
         # ::context[:current_user].page(page).per(limit)
       end
 
